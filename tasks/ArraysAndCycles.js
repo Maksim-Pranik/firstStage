13139937016e63a459f6cf2c054a4e4291ccb84a
Maksim-Pranik <82829188+Maksim-Pranik@users.noreply.github.com>
2021-07-15 18:17:56 +0300

    Update ArraysAndCycles.js

--- tasks/ArraysAndCycles.js
+++ tasks/ArraysAndCycles.js
@@ -1,12 +1,12 @@
 //Вывод фильмов из массива
-let arrayOfFilms = ['Крепкий орешек', 'Очень страшное кино', 'Область тьмы', 'Зона 51', 'Джентельмены'];
+const arrayOfFilms = ['Крепкий орешек', 'Очень страшное кино', 'Область тьмы', 'Зона 51', 'Джентельмены'];
 
 for (const film of arrayOfFilms) {
   console.log(film);
 }
 
 //Массив в строку и обратно в массив
-let autoMakers = ['Peygiot', 'Ford', 'Tesla', 'MG', 'Rimac']
+const autoMakers = ['Peygiot', 'Ford', 'Tesla', 'MG', 'Rimac']
 let stringFromArray = autoMakers.toString()
 let stringToArray = stringFromArray.split(',')
 
@@ -14,7 +14,7 @@ console.log(stringFromArray);
 console.log(stringToArray);
 
 //Добавление слова "Hello" к каждому слову массива
-let namesOfPeopleIKnow = ['Вика', 'Слава', 'Дмитрий', 'Евлампий', 'Аркадий']
+const namesOfPeopleIKnow = ['Вика', 'Слава', 'Дмитрий', 'Евлампий', 'Аркадий']
 
 let grittingsFriends = namesOfPeopleIKnow.map(function (name) {
   return 'Hello ' + name;
@@ -23,43 +23,39 @@ let grittingsFriends = namesOfPeopleIKnow.map(function (name) {
 console.log(grittingsFriends);
 
 //Числовой массив в boolean
-let arrayOfNumbers = [4, 8, 0, 87, 12]
+const arrayOfNumbers = [4, 8, 0, 87, 12]
 let booleanArray = arrayOfNumbers.map(function (num) {
   return Boolean(num)
 })
 console.log(booleanArray);
 
 //Отсортировать массив [1,6,7,8,3,4,5,6] по убыванию
-let numbers = [1, 6, 7, 8, 3, 4, 5, 6];
-numbers.sort(function (a, b) {
-  return a - b;
-});
-numbers.reverse();
+const numbers = [1, 6, 7, 8, 3, 4, 5, 6];
+numbers.sort((a, b) => b - a);
 console.log(numbers)
 
 //Отфильтровать массив [1,6,7,8,3,4,5,6] по значению >3
-let numbersForCompare = [1, 6, 7, 8, 3, 4, 5, 6];
-const ArrayWithNumberMoreThanThree = numbersForCompare.filter(num => num > 3);
-console.log(ArrayWithNumberMoreThanThree);
+const numbersForCompare = [1, 6, 7, 8, 3, 4, 5, 6];
+let arrayWithNumberMoreThanThree = numbersForCompare.filter(num => num > 3);
+console.log(arrayWithNumberMoreThanThree);
 
 //Написать функцию, которая принимает два параметра 
 //- массив и число и выводит индекс элемента массива равный числу
-function GetElement(array, locationNum) {
+function getElement(array, locationNum) {
   console.log(array[locationNum]);
 }
-GetElement(arrayOfFilms, 2)
+getElement(arrayOfFilms, 2)
 
 //Реализовать цикл, который будет выводить число а, пока оно не станет меньше 10
 for (let a = 20; a >= 10; a--) {
   console.log(a);
-
 }
 //Реализовать цикл, который выводит в консоль простые числа
 nextPrime:
 for (let i = 2; i <= 10; i++) {
 
   for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue nextPrime;
+    if (i % j === 0) continue nextPrime;
   }
 
   console.log(i);
